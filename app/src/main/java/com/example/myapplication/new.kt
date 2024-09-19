9d2b0016aa243a9f169a7d9df7bfd383831fb821
UmairKhanDeveloper <uk236393@gmail.com>
2024-09-19 15:48:50 +0500

    Add New Screen

--- app/src/main/java/com/example/myapplication/new.kt
+++ app/src/main/java/com/example/myapplication/new.kt
@@ -1,16 +1,16 @@
 package com.example.myapplication
 
 import android.annotation.SuppressLint
-import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.size
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Category
-import androidx.compose.material.icons.filled.FavoriteBorder
+import androidx.compose.material.icons.filled.Favorite
 import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
@@ -22,6 +22,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.dp
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -31,37 +32,24 @@ fun HomeScreen1() {
     Scaffold(
         topBar = {
             TopAppBar(title = {
-                Text(text = "Home Screen", color = Color.Black)
+                Text(text = "homescreen")
             }, navigationIcon = {
-                Icon(imageVector = Icons.Default.Home, contentDescription = "",tint = Color.Black)
+                Icon(imageVector = Icons.Default.Home, contentDescription = "", )
             }, actions = {
-                Icon(
-                    imageVector = Icons.Default.FavoriteBorder,
-                    contentDescription = "",
-                    tint = Color.Black
-                )
-                Icon(
-                    imageVector = Icons.Default.Category,
-                    contentDescription = "",
-                    tint = Color.Black
-                )
+                Icon(imageVector = Icons.Default.MoreVert, contentDescription = "", )
+                Icon(imageVector = Icons.Default.Favorite, contentDescription = "", )
+            }, colors = TopAppBarDefaults.largeTopAppBarColors(containerColor = Color.Magenta))
 
-            }, colors = TopAppBarDefaults.topAppBarColors(containerColor = Color.Magenta))
         }, floatingActionButton = {
             FloatingActionButton(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Default.Add, contentDescription = "", tint = Color.Black)
+                Icon(imageVector = Icons.Default.Add, contentDescription = "")
 
             }
         }
-    ) {
-        Box(
-            modifier = Modifier
-                .fillMaxSize()
-                .padding(top = it.calculateTopPadding()),
-            contentAlignment = Alignment.Center
-        ) {
 
-            Text(text = "umiar khan", modifier = Modifier.background(color = Color.Magenta))
+    ) {
+        Box(modifier = Modifier.fillMaxSize().padding(top = it.calculateTopPadding()), contentAlignment = Alignment.Center) {
+            Text(text = "umiar khan", color = Color.Red, modifier = Modifier)
         }
 
     }
